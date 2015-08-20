1497b9d21bf5860049eb73930bf0756581fbc3aa
tomkulish <thomas.kulish@gmail.com>
2015-08-20 13:51:29 -0400

    Updated the readme with more information and started Stage 0 and Stage  of the run_analysis.R

--- Readme.md
+++ Readme.md
@@ -1,3 +1,4 @@
 # Coursera Data Science - Getting and Cleanning Data
+## by Thomas Kulish
 
-These are the instructions on how to run the final project and a description of the files.
+The run_analysis.R script creates a reproducable script in which to clean up and create a "tidy" data set from the Human Activity Recognition Using Smartphones Dataset created by Jorge L. Reyes-Ortiz, Davide Anguita, Alessandro Ghio, Luca Oneto. 
--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,7 @@
 #########################################
 # run_analysis.R 
 # 4 Aug 2015
+# NOTE: This has been created to work on a Windows machine (notice backslashes)
 #########################################
 
 # Goals:
@@ -33,4 +34,20 @@ list.files(".\\data")
 # Unzip file
 files <- unzip(destFile, list=TRUE)
 unzip(destFile, exdir="data")
+
+# The files we are looking to play with:
+activityLabelsFile <- "data\\UCI HAR Dataset\\activity_labels.txt"
+featuresFile <- "data\\UCI HAR Dataset\\features.txt"
+
+trainXFile <- "data\\UCI HAR Dataset\\train\\X_train.txt"
+trainYFile <- "data\\UCI HAR Dataset\\train\\y_train.txt"
+testSubjectFile <- "data\\UCI HAR Dataset\\test\\subject_test.tx"
+testXFile <- "data\\UCI HAR Dataset\\test\\X_test.txt"
+testYFile <- "data\\UCI HAR Dataset\\test\\y_test.txt"
+trainSubjectFile <- "data\\UCI HAR Dataset\\train\\subject_train.txt"
+
+#####################
+# Step 2: Merge the data sets to create one dataset
+#####################
+trainX <- read.table(trainXFile)
 
